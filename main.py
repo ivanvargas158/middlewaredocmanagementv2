d00e067b3e9cc8704fa4cbb93e705e531c5d137d
ivargas <ivargas@providencesolutions.io>
2025-08-18 19:05:39 -0400

    add logging

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI,HTTPException,UploadFile, File
+from fastapi import FastAPI
 from app.api.endpoints.ocr import process_files
 from app.api.endpoints.document import manage_files
 from app.utils.docker.download_model import download_model_from_blob 
 from app.utils.global_security import set_predictor
-#comment to test
+import traceback
+
 app = FastAPI()
  
 
@@ -12,12 +13,13 @@ app.include_router(manage_files.router,prefix="/api/v1/document",tags=["Manage f
 
 @app.on_event("startup")
 async def startup_event():
-      # Download ONNX model to a safe folder
-    model_path = download_model_from_blob()
+      # Download ONNX model to a safe folder 
     
     try:
+        model_path = download_model_from_blob()
         set_predictor(model_path=model_path)
     except Exception as e:
-        print(f"Failed to initialize ONNX model: {e}")
+        print("Startup failed with exception:")
+        traceback.print_exc()
         raise
  
